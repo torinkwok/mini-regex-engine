f56f3266c363953d39b4965cc1cdee6a1b7a54ae
Torin Kwok <torin@kwok.im>
2019-04-19 13:51:43 +0800

    Renamed two member properties

--- NFA.java
+++ NFA.java
@@ -2,29 +2,29 @@ import java.util.Vector;
 
 public class NFA
 {
-  public int _start;
-  public int _end;
+  public int start;
+  public int end;
 
-  public final Vector<Object> _transtbl;
+  public final Vector<Object> transtbl;
 
   public NFA( NFA src )
   {
-    this._transtbl = src._transtbl;
-    this._start    = src._start;
-    this._end      = src._end;
+    this.transtbl = src.transtbl;
+    this.start    = src.start;
+    this.end      = src.end;
   }
 
   public NFA( int size, int start, int end )
   {
-    _transtbl = new Vector<Object>( size );
-
-    _start = start;
-    _end   = end;
-
     assert( isLegalState( start ) );
     assert( isLegalState( end ) );
 
-    // Initialize _transtbl with an "empty graph",
+    transtbl = new Vector<Object>( size );
+
+    this.start = start;
+    this.end   = end;
+
+    // Initialize transtbl with an "empty graph",
     // no transitions between its states
 
     for ( int i = 0; i < size; i++ )
@@ -32,11 +32,11 @@ public class NFA
       Vector<Input> row = new Vector<Input>();
       for ( int j = 0; j < size; j++ ) { row.add( Input.NONE ); }
 
-      _transtbl.add( row );
+      transtbl.add( row );
     }
   }
 
-  public int     count()               { return _transtbl.size();      }
+  public int     count()               { return transtbl.size();      }
   public boolean isLegalState( int s ) { return s >= 0 || s < count(); }
 
   public void addTransition( int from, int to, Input in )
@@ -44,21 +44,21 @@ public class NFA
     assert( isLegalState( from ) );
     assert( isLegalState( to ) );
 
-    Vector<Input> row = ( Vector<Input> )_transtbl.get( from );
+    Vector<Input> row = ( Vector<Input> )transtbl.get( from );
     row.setElementAt( in, to );
   }
 
   public void show()
   {
     System.out.println( String.format( "This NFA got %d states: s0 - s%d", count(), count() - 1 ) );
-    System.out.println( String.format( "The initial state is s%d", _start ) );
-    System.out.println( String.format( "The final state is s%d", _end ) );
+    System.out.println( String.format( "The initial state is s%d", start ) );
+    System.out.println( String.format( "The final state is s%d", end ) );
 
     for ( int from = 0; from < count(); from++ )
     {
       for ( int to = 0; to < count(); to++ )
       {
-        Vector<Input> row = ( Vector<Input> )_transtbl.get( from );
+        Vector<Input> row = ( Vector<Input> )transtbl.get( from );
         Input in = row.get( to );
 
         if ( in != Input.NONE )
@@ -79,15 +79,15 @@ public class NFA
     Vector<Input> newrow = new Vector<Input>( count() + 1 );
     for ( int i = 0; i < count(); i++ ) { newrow.add( Input.NONE ); }
 
-    _transtbl.add( newrow );
+    transtbl.add( newrow );
 
     for ( int i = 0; i < count(); i++ )
     {
-      Vector<Input> ri = ( Vector<Input> )_transtbl.get( i );
+      Vector<Input> ri = ( Vector<Input> )transtbl.get( i );
       ri.add( Input.NONE );
     }
 
-    _end++;
+    end++;
   }
 
   /** Renames all the NFA's states:
@@ -105,17 +105,17 @@ public class NFA
       Vector<Input> newrow = new Vector<Input>( count() + shift );
       for ( int j = 0; j < count(); j++ ) { newrow.add( Input.NONE ); }
 
-      _transtbl.insertElementAt( newrow, 0 );
+      transtbl.insertElementAt( newrow, 0 );
     }
 
     for ( int i = 0; i < count(); i++ )
     {
-      Vector<Input> ri = ( Vector<Input> )_transtbl.get( i );
+      Vector<Input> ri = ( Vector<Input> )transtbl.get( i );
       for ( int j = 0; j < shift; j++ ) { ri.insertElementAt( Input.NONE, 0 ); }
     }
 
-    _start += shift;
-    _end   += shift;
+    start += shift;
+    end   += shift;
   }
 
   /** Fills states 0 up to src.count() with src's states.
@@ -129,8 +129,8 @@ public class NFA
     {
       for ( int j = 0; j < srcsz; j++ )
       {
-        Vector<Input> rsrc = ( Vector<Input> )src._transtbl.get( i );
-        Vector<Input> rdst = ( Vector<Input> )dst._transtbl.get( i );
+        Vector<Input> rsrc = ( Vector<Input> )src.transtbl.get( i );
+        Vector<Input> rdst = ( Vector<Input> )dst.transtbl.get( i );
 
         rdst.setElementAt( rsrc.get( j ), j );
       }
@@ -140,19 +140,20 @@ public class NFA
   public void dumpInternalTranstbl()
   {
     System.out.println( "====================" );
-    System.out.println( "Initial State: " + _start );
-    System.out.println( "  Final State: "   + _end );
+    System.out.println( "Initial State: " + start );
+    System.out.println( "  Final State: " + end );
     System.out.println( "--------------------" );
     System.out.println();
 
     for ( int i = 0; i < count(); i++ )
     {
-      Vector<Input> r = ( Vector<Input> )_transtbl.get( i );
+      Vector<Input> r = ( Vector<Input> )transtbl.get( i );
 
       for ( int j = 0; j < count(); j++ )
       {
         char c;
         Input in = r.get( j );
+
         if      ( in == Input.NONE ) { c = '-';  }
         else if ( in == Input.EPS  ) { c = 'E';  }
         else                         { c = in.v; }
@@ -169,21 +170,21 @@ public class NFA
     nfa1.shiftStates( 1 );            // make room for the new initial state
     nfa2.shiftStates( nfa1.count() ); // make room for nfa1's states
 
-    NFA nfaAlter = new NFA( nfa2 );
-    nfaAlter.fillStates( nfa1 );
-    nfaAlter.shiftStates( 1 );
+    NFA nfaUnion = new NFA( nfa2 );
+    nfaUnion.fillStates( nfa1 );
+    nfaUnion.shiftStates( 1 );
 
-    nfaAlter.addTransition( 0, nfa1._start, Input.EPS );
-    nfaAlter.addTransition( 0, nfa2._start, Input.EPS );
-    nfaAlter._start = 0;
+    nfaUnion.addTransition( 0, nfa1.start, Input.EPS );
+    nfaUnion.addTransition( 0, nfa2.start, Input.EPS );
+    nfaUnion.start = 0;
 
-    nfaAlter.appendEmptyState();
-    nfaAlter._end = nfaAlter.count() - 1;
+    nfaUnion.appendEmptyState();
+    nfaUnion.end = nfaUnion.count() - 1;
 
-    nfaAlter.addTransition( nfa1._end, nfaAlter._end, Input.EPS );
-    nfaAlter.addTransition( nfa2._end, nfaAlter._end, Input.EPS );
+    nfaUnion.addTransition( nfa1.end, nfaUnion.end, Input.EPS );
+    nfaUnion.addTransition( nfa2.end, nfaUnion.end, Input.EPS );
 
-    return nfaAlter;
+    return nfaUnion;
   }
 
   public NFA buildNFAConcatenation( NFA nfa1, NFA nfa2 )
@@ -206,7 +207,7 @@ public class NFA
     // Set the new initial state (the final state stays nfa2's
     // final state, and was already copied)
     //
-    nfaConcat._start = nfa1._start;
+    nfaConcat.start = nfa1.start;
 
     return nfaConcat;
   }
@@ -219,13 +220,13 @@ public class NFA
     nfaKleeneStar.fillStates( nfa );
     nfaKleeneStar.appendEmptyState();
 
-    nfaKleeneStar._start = 0;
-    nfaKleeneStar._end = nfaKleeneStar.count() - 1;
+    nfaKleeneStar.start = 0;
+    nfaKleeneStar.end = nfaKleeneStar.count() - 1;
 
-    nfaKleeneStar.addTransition( 0, nfa._start, Input.EPS );
-    nfaKleeneStar.addTransition( 0, nfaKleeneStar._end, Input.EPS );
-    nfaKleeneStar.addTransition( nfa._end, nfa._start, Input.EPS );
-    nfaKleeneStar.addTransition( nfa._end, nfaKleeneStar._end, Input.EPS );
+    nfaKleeneStar.addTransition( 0, nfa.start, Input.EPS );
+    nfaKleeneStar.addTransition( 0, nfaKleeneStar.end, Input.EPS );
+    nfaKleeneStar.addTransition( nfa.end, nfa.start, Input.EPS );
+    nfaKleeneStar.addTransition( nfa.end, nfaKleeneStar.end, Input.EPS );
 
     return nfaKleeneStar;
   }

f11181cef2ab0091d40661d0f7903a1112c5268d
Torin Kwok <torin@kwok.im>
2019-04-21 14:02:59 +0800

    A little format

--- AdHocStack.java
+++ AdHocStack.java
@@ -3,15 +3,15 @@ public class AdHocStack<T>
   private T[] _a = (T[]) new Object[1];
   private int _N = 0;
 
-  public boolean isEmpty()  { return _N == 0;     }
-  public int     size()     { return _N;          }
+  public boolean isEmpty()  { return _N == 0;   }
+  public int     size()     { return _N;        }
   public int     capacity() { return _a.length; }
 
   public void _resize( int new_sz )
   {
     T[] buffer = (T[]) new Object[ new_sz ];
     for ( int i = 0; i < _N; i++ )
-      buffer[i ] = _a[i];
+      buffer[i] = _a[i];
     _a = buffer;
   }
 
@@ -19,7 +19,7 @@ public class AdHocStack<T>
   {
     if ( size() == capacity() ) _resize( capacity() * 2 );
     _a[_N++] = ele;
-  }
+  }p
 
   public T pop()
   {

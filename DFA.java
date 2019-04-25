2e9ffcc2184cb6002588f2e1850d8466a7f385c2
Torin Kwok <torin@kwok.im>
2019-04-25 15:19:23 +0800

    Made DFA.java class take advantage of our brand new State.java class

--- DFA.java
+++ DFA.java
@@ -2,35 +2,35 @@ import java.util.*;
 
 public class DFA implements Cloneable
 {
-  public int          start;
-  public Set<Integer> ends = new HashSet<>();
+  public State      start = State.ZERO;
+  public Set<State> ends = new HashSet<>();
 
-  // The DFA's transition table is implemented with a Map, that
-  // maps (State, Input) pairs to states. For example, (S1, i)
-  // will be mapped to S2 if input i in sate S1 leads to state
-  // S2.
-  //
-  private final Map<Map<Integer, Input>, Integer> transtbl;
+  /** The DFA's transition table is implemented with a Map, that
+   *  maps (State, Input) pairs to states. For example, (S1, i)
+   *  will be mapped to S2 if input i in sate S1 leads to state
+   *  S2.
+   */
+  private final Map<Map<State, Input>, State> transtbl;
 
   @Override
   public DFA clone() { return new DFA( this ); }
 
-  public DFA()
-  {
-    this.transtbl = new HashMap<>();
-  }
+  public DFA() { this.transtbl = new HashMap<>(); }
 
   public DFA( DFA src )
   {
-    Map<Map<Integer, Input>, Integer> cloned_transtbl = new HashMap<>();
+    Map<Map<State, Input>, State> cloned_transtbl = new HashMap<>();
 
-    for ( Map.Entry<Map<Integer, Input>, Integer> entry : src.transtbl.entrySet() )
+    for ( Map.Entry<Map<State, Input>, State> entry : src.transtbl.entrySet() )
       cloned_transtbl.put( entry.getKey(), entry.getValue() );
 
     this.transtbl = cloned_transtbl;
   }
 
-  public void addTransition( int from, int to, Input in )
+  public void addTransition( int   from, int   to, Input in ) { addTransition( new State( from ), new State( to ), in ); }
+  public void addTransition( int   from, State to, Input in ) { addTransition( new State( from ), to,              in ); }
+  public void addTransition( State from, int   to, Input in ) { addTransition( from,              new State( to ), in ); }
+  public void addTransition( State from, State to, Input in )
   {
     if ( in == Input.NONE ) return;
     if ( in == Input.EPS  ) throw new RuntimeException( "DFA does not support NULL input" );
@@ -38,15 +38,16 @@ public class DFA implements Cloneable
     this.transtbl.put( new HashMap(){{ put( from, in ); }}, to );
   }
 
-  private boolean _simulate( int currentState, String in )
+  private boolean _simulate( State currentState, String in )
   {
     if ( in.length() == 0 )
       return this.ends.contains( currentState );
 
     Input c = new Input( in.charAt( 0 ) );
 
-    Map<Integer, Input> from_pair = new HashMap(){{ put( currentState, c ); }};
-    Integer to_state = this.transtbl.get( from_pair );
+    Map<State, Input> from_pair = new HashMap(){{ put( currentState, c ); }};
+    State to_state = this.transtbl.get( from_pair );
+
     if ( to_state != null )
       return _simulate( to_state, in.substring( 1 ) );
 
@@ -61,11 +62,11 @@ public class DFA implements Cloneable
   public void show()
   {
     System.out.println( "DFA start state: " + this.start );
-    System.out.println( "DFA fianl state(s): {" );
+    System.out.println( "DFA final state(s): {" );
 
     if ( this.ends.size() > 0 )
-      for ( int state : this.ends )
-        System.out.println( "\t" + state + "," );
+      for ( State sf : this.ends )
+        System.out.println( "\t" + sf + "," );
 
     System.out.println( "}" );
   }
@@ -80,9 +81,9 @@ public class DFA implements Cloneable
     dfa.addTransition( 1, 2, new Input( 's' ) );
     dfa.addTransition( 0, 0, new Input( 'b' ) );
 
-    dfa.ends.add( 0 );
-    dfa.ends.add( 1 );
-    dfa.ends.add( 2 );
+    dfa.ends.add( State.ZERO );
+    dfa.ends.add( new State( 1 ) );
+    dfa.ends.add( new State( 2 ) );
 
     try {
       dfa.addTransition( 1, 2, Input.EPS );
